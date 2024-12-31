7a80f9e90a75d0a46014391229434796e2a2f865
Jake Madlon <jakemadlon@gmail.com>
2024-12-31 12:19:43 +0100

    Added AutoMapper

--- stagevoorbereiding-API/stagevoorbereiding-API/Program.cs
+++ stagevoorbereiding-API/stagevoorbereiding-API/Program.cs
@@ -1,17 +1,28 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using AutoMapper;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 
-builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
-{
-});
-
 builder.Services.AddControllers();
 
-var app = builder.Build();
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
+{
+    var config = new MapperConfiguration(cfg =>
+    {
+        cfg.AddProfile<MappingProfile>();
+    });
 
+    containerBuilder.RegisterInstance(config).AsSelf().SingleInstance();
+
+    containerBuilder
+        .Register(ctx => config.CreateMapper())
+        .As<IMapper>()
+        .SingleInstance();
+});
+
+var app = builder.Build();
 app.MapControllers();
 app.Run();
--- stagevoorbereiding-API/stagevoorbereiding-API/obj/Debug/net9.0/stagevoorbereiding-API.AssemblyInfo.cs
+++ stagevoorbereiding-API/stagevoorbereiding-API/obj/Debug/net9.0/stagevoorbereiding-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("stagevoorbereiding-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7514d3a66384c90c912e87361f206bc880229d5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+882b3fc1bb52df0e36721643be9da3d9ddcf50b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("stagevoorbereiding-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("stagevoorbereiding-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
